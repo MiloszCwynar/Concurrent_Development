e543c55c82100897c67c830fbbf4b703ef34b4b5
milo <gamingt334@gmail.com>
2026-09-21 10:15:21 +0100

    mutex and semaphore completed

---
+++ LabOne/semaphore.go
@@ -0,0 +1,32 @@
+package main
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+//make struct containing channel
+//add init, acquire and release
+type semaphore struct {
+	theCounter chan struct{}
+}
+
+funcAcquite(sem *Semaphore)
+func main() {
+	maxGoroutines:=5
+	semaphore:=make(chan struct{}, maxGoroutines)
+
+	var wg sync.WaitGroup
+	for i:=0;i<20;i++{
+		wg.Add(1)
+		go func(i int){
+			defer wg.Done()
+			semaphore<-struct{}{}
+			defer func() {<-semaphore}()
+
+			//simulate a task
+			fmt.Println("running task %d\n", i)
+			time.Sleep(2 *time.Second)
+		}(i)
+	}
+	wg.Wait()
+}
